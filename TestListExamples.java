25a45479872385a5d30e4634a9fa777a3988dbee
salnav <sal.navroz@gmail.com>
2023-11-28 11:16:07 -0800

    Grade stuff comiitted

--- TestListExamples.java
+++ TestListExamples.java
@@ -2,6 +2,7 @@ import static org.junit.Assert.*;
 import org.junit.*;
 import java.util.Arrays;
 import java.util.List;
+import java.util.ArrayList;
 
 class IsMoon implements StringChecker {
   public boolean checkString(String s) {
@@ -17,5 +18,16 @@ public class TestListExamples {
     List<String> merged = ListExamples.merge(left, right);
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
+  }
+
+
+@Test(timeout = 500)
+public void testFilter() {
+    List<String> strs = new ArrayList<>();
+    strs.add("a");
+    strs.add("b");
+    strs.add("apple");
+    List<String> filtered = ListExamples.filter(new IsMoon(), strs);
+    assertEquals(filtered, Arrays.asList("a", "apple"));
   }
 }
--- grade.sh
+++ grade.sh
@@ -1,3 +1,4 @@
+
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
 rm -rf student-submission
@@ -7,6 +8,50 @@ mkdir grading-area
 
 git clone $1 student-submission
 echo 'Finished cloning'
+
+if  [[ -f student-submission/ListExamples.java ]]
+then 
+    echo ""
+else
+     echo -e "File not found.\nFinal score: 0%"
+    exit
+fi
+
+cp student-submission/* grading-area/
+cp TestListExamples.java grading-area/
+
+
+# javac -cp $CPATH *.java
+error_output=$(javac -cp $CPATH grading-area/*.java 2>&1)
+if [[ $? -ne 0 ]]
+then 
+    echo -e "There was an error:\n$error_output\nFinal score: 0%"
+else
+    cd grading-area
+    java -cp ../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar:. org.junit.runner.JUnitCore TestListExamples | grep -m 1 -A1 "JUnit" > results.txt
+
+    second_line=$(sed -n '2p' results.txt)
+
+    # Count the number of dots in the second line
+    total=$(grep -o '\.' <<< "$second_line" | wc -l)
+
+    # Count the number of 'E's in the second line
+    e_count=$(grep -o 'E' <<< "$second_line" | wc -l)
+
+    # Output the counts
+    # echo "Number of dots: $dot_count"
+    # echo "Number of E's: $e_count"
+
+    success=$(( total - e_count  ))
+    percentage=$((success * 100 / total))
+    echo "Final score: $percentage%"
+fi
+
+
+
+
+
+
 
 
 # Draw a picture/take notes on the directory structure that's set up after
